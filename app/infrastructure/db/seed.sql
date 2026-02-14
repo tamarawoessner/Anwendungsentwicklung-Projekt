86aa93205f81afe84091979e2c78c3b30473600a
Bennet Klassen <bennet.klassen@hansgrohe.com>
2026-02-14 18:16:04 +0100

    Add seeded DB dump

--- app/infrastructure/db/seed.sql
+++ app/infrastructure/db/seed.sql
@@ -1,1 +1,26 @@
-pg_dump: error: no matching schemas were found
+--
+-- PostgreSQL database dump
+--
+
+\restrict mk1vkLea8QSQBJLQNKOWMMlA55iaERcc3JkxI1DR6oWLyjlEshnUBbqDD5c0PlH
+
+-- Dumped from database version 16.12 (Debian 16.12-1.pgdg13+1)
+-- Dumped by pg_dump version 16.12 (Debian 16.12-1.pgdg13+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict mk1vkLea8QSQBJLQNKOWMMlA55iaERcc3JkxI1DR6oWLyjlEshnUBbqDD5c0PlH
+
