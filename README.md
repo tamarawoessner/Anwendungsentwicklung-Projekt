4f0dc8dd5f5cbb79b3a3b2392389d55d63cc4947
Bennet Klassen <bennet.klassen@hansgrohe.com>
2026-02-28 18:26:12 +0100

    start infrastructure development

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Anwendungsentwicklung-Projekt
+
+Setup:
+1. .env.example → .env kopieren
+2. docker compose pull
+3. docker compose up -d
---
+++ app/infrastructure/db/connection.py
@@ -0,0 +1,33 @@
+import psycopg2
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Database connection settings
+DB_HOST = "db"
+DB_PORT = 5432
+DB_NAME = "app"
+DB_USER = "app"
+DB_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+
+
+# Connect to PostgreSQL
+def connect_to_db():
+    try:
+        conn = psycopg2.connect(
+            host=DB_HOST,
+            port=DB_PORT,
+            database=DB_NAME,
+            user=DB_USER,
+            password=DB_PASSWORD,
+        )
+        print("Connection successful")
+        return conn
+    except Exception as e:
+        print(f"Error connecting to database: {e}")
+        return None
+
+
+if __name__ == "__main__":
+    connect_to_db()
