b3a2ae51bbe8a3c6803c89836f22963013ffd32b
janis-lht <janis.leonhardt02@gmail.com>
2026-03-01 18:30:36 +0100

    frontend init

---
+++ frontend/src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="app-wrapper">
+    </div>
+</template>
+
+<style scoped>
+.app-wrapper {
+  margin: 0;
+  padding: 0;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  overflow: hidden;
+}
+</style>
